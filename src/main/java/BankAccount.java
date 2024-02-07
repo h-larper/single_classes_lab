20877171abbb61b40998b3a8b675f77f5757587a
Laura Harper <lauraharper95@gmail.com>
2024-02-07 15:08:10 +0000

    added first getter test

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,19 +1,21 @@
 public class BankAccount {
 
-    private String firstName;               // Creating instance variables (?)
+    private String firstName;               // Creating instance variables
     private String lastName;
     private String dateOfBirth;
     private int accountNumber;
     private int balance;
 
 
-    public BankAccount(String inputFirstName, String inputLastName, String inputdateOfBirth, int inputAccountNumber, int inputBalance) {               //Creating constructor function (?)
+    public BankAccount(String inputFirstName, String inputLastName, String inputDateOfBirth, int inputAccountNumber) {               //Creating constructor function (?)
         this.firstName = inputFirstName;
         this.lastName = inputLastName;
-        this.dateOfBirth = inputdateOfBirth;
+        this.dateOfBirth = inputDateOfBirth;
         this.accountNumber = inputAccountNumber;
         this.balance = 0;
     }
+
+
 
 //    GETTERS & SETTERS:
 
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,9 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BankAccountTest {
 
     private BankAccount bankAccount;            //declaring variable bankAccount
 
-    
+//    Testing Getters & Setters:
+
+    @Test
+    public void canGetFirstName() {
+        BankAccount bankAccount = new BankAccount("Laura", "Harper", "171095", 2000);    // calling method
+        String result = bankAccount.getFirstName();         // lower case bankAccount (object) BankAccount (class)
+        assertThat(result).isEqualTo("Laura");
+    }
 
 }
