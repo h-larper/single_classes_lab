517dad2553909a08a9e102e89f3ed3f2386101bd
Laura Harper <lauraharper95@gmail.com>
2024-02-07 15:41:19 +0000

    firstname and lastname getter and setter tests written and passed

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -7,13 +7,39 @@ public class BankAccountTest {
 
     private BankAccount bankAccount;            //declaring variable bankAccount
 
+
+
 //    Testing Getters & Setters:
 
     @Test
     public void canGetFirstName() {
-        BankAccount bankAccount = new BankAccount("Laura", "Harper", "171095", 2000);    // calling method
+        BankAccount bankAccount = new BankAccount("Howard", "Moon", "010175", 2000);    // calling method
         String result = bankAccount.getFirstName();         // lower case bankAccount (object) BankAccount (class)
         assertThat(result).isEqualTo("Laura");
     }
+
+    @Test
+    public void canSetFirstName() {              // before testing, need to test the get method 1st. + void so doesn't return anything, but changes the name called by get.
+        BankAccount bankAccount = new BankAccount("Howard", "Moon", "010175", 2000);
+        bankAccount.setFirstName("Vince");                   // can't call inside variable bc doesn't return anything
+        String result = bankAccount.getFirstName();         // able to return string
+        assertThat(result).isEqualTo("Vince");
+    }
+
+    @Test
+    public void canGetLastName() {
+        BankAccount bankAccount = new BankAccount("Howard", "Moon", "010175", 2000);
+        String result = bankAccount.getLastName();
+        assertThat(result).isEqualTo("Moon");
+    }
+
+    @Test
+    public void canSetLastName() {
+        BankAccount bankAccount = new BankAccount("Howard", "Moon", "010175", 2000);
+        bankAccount.setLastName("Noir");
+        String result = bankAccount.getLastName();
+        assertThat(result).isEqualTo("Noir");
+    }
+
 
 }
